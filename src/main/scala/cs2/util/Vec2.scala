cd4726bd932ef2614840f69c41f991644e65a055
2016-08-23 <hmwizaru@xena03.cs.trinity.edu>
2019-02-06 13:36:31 -0600

    modified Vec2

--- src/main/scala/cs2/util/Vec2.scala
+++ src/main/scala/cs2/util/Vec2.scala
@@ -1,44 +1,90 @@
 package cs2.util
 
-class Vec2 (var x:Double, var y:Double) {
-	/* UNCOMMENT THE CODE BELOW AND COMPLETE THE METHODS
+class Vec2 private(var _x:Double, var _y:Double) {
+  
+  def x = _x
+  def y = _y
 
 	//Default constructor creates a Vec2 of zeroes
-  def this()
+  def this() = {
+    this(0.0 , 0.0)
+  }
 
   //Copy constructor create a "deep" copy of the input object
-  def this(toCopy:Vec2)
+  def this(toCopy:Vec2) = {
+    this(toCopy._x, toCopy._y)
+  }
 
   //Provided toString method simplifies printing, e.g. println(vec.toString) OR println(vec)
 	override def toString():String = "("+x+","+y+")"
 
 	//Methods for addition and subtraction of vectors
-	def + (other:Vec2):Vec2
-	def - (other:Vec2):Vec2
+	def + (other:Vec2):Vec2 = new Vec2(_x + other._x, y + other._y)
+	def - (other:Vec2):Vec2 = new Vec2(_x - other._x, y - other._y)
 
-	def += (other:Vec2)
-	def -= (other:Vec2)
+	def += (other:Vec2): Vec2 = {
+	  _x += other._x
+	  _y += other._y 
+	  this 
+	}
+	def -= (other:Vec2): Vec2 = {
+	  _x -= other._x
+	  _y -= other._y
+	  this
+	}
 
 	//Methods for multiplication and division of vectors by a scalar (non-vector)
-	def * (scalar:Double):Vec2
-	def / (scalar:Double):Vec2
+	def * (scalar:Double):Vec2 = new Vec2(this.x*scalar, this.y*scalar)
+	def / (scalar:Double):Vec2 = new Vec2(this.x/scalar, this.y/scalar)
 
-	def *= (scalar:Double)
-	def /= (scalar:Double)
+	def *= (scalar:Double): Vec2 = {
+	  _x *= scalar
+	  _y *= scalar
+	  this
+	}
+	def /= (scalar:Double): Vec2 = {
+	  _x /= scalar
+	  _y /= scalar
+	  this
+	}
 
 	//Methods to determine the length of a vector (magnitude and length should return the same value)
-	def magnitude():Double
-	def length():Double
+	def magnitude():Double = math.sqrt(_x*_x + _y*_y)
+	def length():Double = math.sqrt(_x*_x + _y*_y)
 
 	//Method to return a new vector that is in the same direction, but length 1
-	def normalize():Vec2
-	def unary_~()
+
+	def unary_~() = new Vec2(-1*_x,-1*_y)
 
 	//Methods to calculate the dot product, and determine the angle between 2 vectors
-	def dot(other:Vec2):Double
-	def **(other:Vec2):Double
+	def dot(other:Vec2):Double = this._x*other._x + this._y*other._y
+	def **(other:Vec2):Double = this._x*other._x - this._y*other._y
 
-	def angleBetween(other:Vec2):Double
-	def <>(other:Vec2):Double
-	*/
+	def angleBetween(other:Vec2):Double = math.cos(dot(this)*dot(other)/this.length*other.length)
+
+	def <>(other:Vec2):Double = math.cos(dot(this)*dot(other)/this.length*other.length)
 }
+
+
+object Vec2 {
+  
+  def main(args: Array[String]): Unit = {
+    val v1 = new Vec2(1,2)
+    val v2 = new Vec2(2,2)
+    v1 * 2
+    println(v1)
+  }
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
